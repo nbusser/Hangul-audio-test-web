5c1f053031f1a25e702b8131f20c9c9c259ae8ec
Nicolas BUSSER <nicolas.busser67@gmail.com>
2022-03-12 15:47:43 +0100

    add template for vue app

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,30 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <button>Listen</button>
+  <div class="hangul">
+    <button v-if="hidden" @click="reveal">?</button>
+    <button v-else>{{ hangul }}</button>
+    <p v-if="!hidden">{{ transcript }}</p>
+  </div>
+  <button @click="nextHangul">Next</button>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue';
-
 export default {
   name: 'App',
-  components: {
-    HelloWorld,
+  data: () => ({
+    hangul: null,
+    transcript: null,
+    audio: null,
+    hidden: true,
+  }),
+  methods: {
+    reveal() {
+      this.hidden = false;
+    },
+    nextHangul() {
+      // TODO: call API
+      this.hidden = true;
+    },
   },
 };
 </script>
@@ -22,5 +37,9 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+
+.hangul {
+  text-align: center;
 }
 </style>
