8196a4801403f16e259ce60ad3a69d2f130803c0
Nicolas BUSSER <nicolas.busser67@gmail.com>
2022-03-16 19:36:12 +0100

    fix wrong random in generate hangul (harmless)

--- src/getHangul.js
+++ src/getHangul.js
@@ -1,7 +1,10 @@
+// https://gist.github.com/hyunjun/03eed685035a22fdaa26#file-hangul_phoneme-py
+// https://github.com/bluedisk/hangul-toolkit/blob/f36b534ee339263fb72e687b732697cc7ed290dc/hgtk/letter.py
+
 import Aromanize from 'aromanize';
 
 const randInt = (min, max) => Math.floor(
-  Math.random() * (max - min) + min,
+  Math.random() * (max - min),
 );
 
 export default (jongsungFreq = 0.5) => {
